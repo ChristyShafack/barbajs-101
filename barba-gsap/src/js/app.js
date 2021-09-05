86aaf0ed787d39228c28d9ec70550057ee283ad9
christyshafack <christyshafack@gmail.com>
2021-09-05 03:10:58 +0100

    Transition for detail page

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -4,16 +4,23 @@ import { revealProject, leaveToProject, leaveFromProject, animationEnter, animat
 
 const resetActiveLink = () => gsap.set('a.is-active span', {
   xPercent: -100,
-  transformOrigin: 'left'
+  transformOrigin: 'left',
 });
 
 barba.init({
   transitions: [
     {
       name: 'detail',
+      to: {
+        namespace: ['detail'],
+      },
       once({ next }) {
+        revealProject(next.container);
+      },
+      leave: ({ current }) => leaveToProject(current.container),
+      enter({ next }) {
         revealProject(next.container)
-      }
+      },
     },
     {
       name: 'general-transition',
@@ -24,15 +31,16 @@ barba.init({
           yPercent: 100,
           stagger: 0.2,
           ease: 'power1.out',
-          onComplete: () => animationEnter(next.container)
+          onComplete: () => animationEnter(next.container),
         });
       },
+
       // we need to wait for this animation to finish before enter animation plays
       leave: ({ current }) => animationLeave(current.container),
       enter({ next }) {
         animationEnter(next.container);
       },
-    }
+    },
   ],
 });
 
