8508f32dbf0ddbbf330d12b9fbc0b227136888b8
christyshafack <christyshafack@gmail.com>
2021-09-05 01:41:48 +0100

    AnimationLeaveTimeline

--- barba-gsap/src/js/animations/animationLeave.js
+++ barba-gsap/src/js/animations/animationLeave.js
@@ -1,11 +1,20 @@
 import gsap from 'gsap';
 const animationLeave = (container) => {
-  return gsap.to(container, {
-    autoAlpha: 0,
-    duration: 0.7,
-    clearProps: 'all',
-    ease: 'none',
+  const activeLink = container.querySelector('a.is-active span');
+  const images = container.querySelectorAll('.image');
+  const img = container.querySelectorAll('.img');
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 0.4,
+      ease: 'power1.in',
+    },
   });
+
+  tl
+    .to(activeLink, { xPercent: 101 }, 0)
+
+  return tl;
 };
 
 export default animationLeave;
